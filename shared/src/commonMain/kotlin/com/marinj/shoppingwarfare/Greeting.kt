1769c437193a2c4d22a11758e66407dc703028f4
mjuric00@fesb.hr <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>
2021-06-06 21:59:00 +0200

    Initial commit

---
+++ shared/src/commonMain/kotlin/com/marinj/shoppingwarfare/Greeting.kt
@@ -0,0 +1,7 @@
+package com.marinj.shoppingwarfare
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/marinj/shoppingwarfare/Platform.kt
@@ -0,0 +1,5 @@
+package com.marinj.shoppingwarfare
+
+expect class Platform() {
+    val platform: String
+}
